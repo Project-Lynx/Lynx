6179828cd27a6772dd2a5fdd152a5c19581bd981
Antonio Hickey <64357439+antonio-hickey@users.noreply.github.com>
2021-07-30 03:38:29 -0400

    Delete dash_app.py

--- dash_app.py
+++
@@ -1,7 +0,0 @@
-#------------------------------------
-import dash
-#------------------------------------
-app = dash.Dash(__name__,  meta_tags=[{"name": "viewport", "content": "width=device-width"}],
-                suppress_callback_exceptions=True, assets_folder='app/assets')
-server = app.server
-#------------------------------------
