357aa5a398319868cd3d083337d99a30e0c1d1e8
Antonio Hickey <antoniohickey99@gmail.com>
2021-08-16 06:42:08 -0400

    linter

--- app/backend/market_data.py
+++ app/backend/market_data.py
@@ -1,16 +1,18 @@
-from app.util import links
-
-from flask import jsonify
-import requests as req
 import json
+
+import requests as req
+from flask import jsonify
+
+from app.util import links
 
 
 def get_quotes(symbols):
     endpoint = '/get-quotes'
     response = req.post(links.dReserve + endpoint, symbols)
     response = json.loads(response.text)
-    
+
     return jsonify(response), 200
+
 
 def get_hist(symbol):
     endpoint = '/get-hist'
