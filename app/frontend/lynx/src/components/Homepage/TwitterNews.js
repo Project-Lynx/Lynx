e28fa0f7c2390a71b7bed020bcde52a8dcbe306f
Antonio Hickey <antoniohickey99@gmail.com>
2021-09-06 01:53:00 -0400

    minor styling changes

--- app/frontend/lynx/src/components/Homepage/TwitterNews.js
+++ app/frontend/lynx/src/components/Homepage/TwitterNews.js
@@ -2,17 +2,17 @@
     Twitter list for news
 
     Default list: https://twitter.com/i/lists/1430053486146859046
-
     You can make your own list if you have a twitter account
     just edit "ownerScreenName" and "id", the id will be the
     end of the link of your list for example the id to the
     default list above would be: 1430053486146859046
 */
 import { Timeline } from 'react-twitter-widgets';
-import './../../assets/css/Homepage/twitter.css';
+import './../../assets/css/Homepage/Twitter.css';
+
 
 const TwitterNews = () => {
-    
+
     return (
         <div>
             <Timeline
@@ -21,11 +21,11 @@ const TwitterNews = () => {
                     ownerScreenName: "ProjectLynx_", // Your profile name
                     id: "1430053486146859046" // Your list id (see above)
                 }}
-                options={{ 
+                options={{
                     chrome: "noheader, nofooter",
-                    theme: "dark", 
-                    width: "385", 
-                    height: "352" 
+                    theme: "dark",
+                    width: "385",
+                    height: "300"
                 }}
             />
         </div>
