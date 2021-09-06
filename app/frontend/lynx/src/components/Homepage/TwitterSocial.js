100743ee635f4710edf3bf1ec191899bb31ae151
Antonio Hickey <antoniohickey99@gmail.com>
2021-09-06 01:53:39 -0400

    minor styling changes

--- app/frontend/lynx/src/components/Homepage/TwitterSocial.js
+++ app/frontend/lynx/src/components/Homepage/TwitterSocial.js
@@ -1,18 +1,18 @@
 /*
     Twitter list for social/comedy
-    
-    Default list: https://twitter.com/i/lists/1430230272558223360
 
+    Default list: https://twitter.com/i/lists/1430230272558223360
     You can make your own list if you have a twitter account
     just edit "ownerScreenName" and "id", the id will be the
     end of the link of your list for example the id to the
     default list above would be: 1430230272558223360
 */
 import { Timeline } from 'react-twitter-widgets';
-import './../../assets/css/Homepage/twitter.css';
+import './../../assets/css/Homepage/Twitter.css';
+
 
 const TwitterSocial = () => {
-    
+
     return (
         <div>
             <Timeline
@@ -21,11 +21,11 @@ const TwitterSocial = () => {
                     ownerScreenName: "ProjectLynx_", // Your profile name
                     id: "1430230272558223360" // Your list id (see above)
                 }}
-                options={{ 
+                options={{
                     chrome: "noheader, nofooter",
-                    theme: "dark", 
-                    width: "385", 
-                    height: "352" 
+                    theme: "dark",
+                    width: "385",
+                    height: "300"
                 }}
             />
         </div>
