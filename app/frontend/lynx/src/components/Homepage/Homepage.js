e9b8f4c5aaf4f98a39648145bed1f9143f099306
Antonio Hickey <antoniohickey99@gmail.com>
2021-09-19 20:38:18 -0400

    Cleaned up for reusable Meta functions

--- app/frontend/lynx/src/components/Homepage/Homepage.js
+++ app/frontend/lynx/src/components/Homepage/Homepage.js
@@ -1,20 +1,22 @@
-import NavBar from './../NavBar.js'
-import NewsFeed from './NewsFeed.js'
-import QouteBoard from './QouteBoard.js'
+import NavBar from './../Meta/NavBar.js'
+import NewsFeed from './../Meta/NewsFeed.js'
+import QouteBoard from './../Meta/QouteBoard.js'
 import Notes from './Notes.js'
 import Events from './Events.js'
 import Graph from './Graph.js'
 import SessionMap from './SessionMap.js'
 
-import './../../assets/css/Homepage.css'
-import './../../assets/css/Bootstrap.css'
+import './../../assets/css/Homepage/Homepage.css'
+import './../../assets/css/Meta/Bootstrap.css'
 
 
 function Home() {
   return (
     <div className="container">
-
-      {NavBar()}
+      
+      <div id="navbar">
+        {NavBar()}
+      </div>
 
       <div id="newsfeed">
           {NewsFeed()}
