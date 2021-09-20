6d20a3e108b9412527d88bef1b1187a11c189900
Antonio Hickey <antoniohickey99@gmail.com>
2021-09-19 20:43:13 -0400

    Added YieldCurve route + css imports

--- app/frontend/lynx/src/routes.js
+++ app/frontend/lynx/src/routes.js
@@ -4,10 +4,12 @@ import { Route } from 'react-router';
 
 import Home from './components/Homepage/Home';
 import App from './components/Landingpage/App';
+import YieldCurve from './components/YieldCurve/root'
 
 
 export default (
   <Route path="/" component={App}>
     <Route path="/home" component={Home} />
+    <Route path="/YieldCurve" component={YieldCurve} />
   </Route>
 );
