99af6aabfcc9016e7f9ba393d3747fe98a691d60
Antonio Hickey <antoniohickey99@gmail.com>
2021-09-24 22:03:37 -0400

    Refactored into smaller components

--- app/backend/repositories/notes.py
+++ app/backend/repositories/notes.py
@@ -1,13 +1,25 @@
 import csv
 import datetime as dt
+from typing import Any
 
 from app.backend.util import conversion
 
 
-def update(data):
-    timestamp = dt.datetime.now().strftime("%m/%d/%Y %H:%M")
+# Export saved notes data
+def export() -> Any:
+    return conversion.csv_to_json('app/backend/data/notes.csv')
 
-    data = list(data.split("{"))
+
+# Handle logic behind daving notes
+def handler(data: str) -> None:
+    data_list = list(data.split("{"))
+    parsed = parse(data_list)
+    save(parsed)
+
+
+# Parse saved notes data
+def parse(data: list) -> list:
+    timestamp = dt.datetime.now().strftime("%m/%d/%Y %H:%M")
     types = []
     content = []
     for i in data:
@@ -18,21 +30,18 @@ def update(data):
             content_base = i[end+content_start+2:]
             content_end = content_base.find('"')
             content_text = content_base[:content_end]
-
             content.append(content_text)
-
-    column_names = ["Timestamp", "Type", "Content"]
     rows = []
     for idx in enumerate(types):
         rows.append([timestamp, idx[1], content[idx[0]]])
+    return rows
 
+
+# Save parsed notes data
+def save(rows: list) -> None:
+    columns = ["Timestamp", "Type", "Content"]
     with open('app/backend/data/notes.csv', 'w') as csvfile:
         csvwriter = csv.writer(csvfile)
-        csvwriter.writerow(column_names)
-
+        csvwriter.writerow(columns)
         for row in rows:
             csvwriter.writerow(row)
-
-
-def export():
-    return conversion.csv_to_json('app/backend/data/notes.csv')
